29d94a9e8efa086125587a26e0c811df1c27716e
Brian <hercemer42@hotmail.com>
2025-11-06 07:22:29 +0100

    typescript fix

--- plugins/core/main/pluginWorker/types/messageValidation.ts
+++ plugins/core/main/pluginWorker/types/messageValidation.ts
@@ -32,7 +32,7 @@ export const PluginManifestSchema = z.object({
   name: z.string(),
   version: z.string(),
   displayName: z.string(),
-  description: z.string(),
+  description: z.string().optional(),
   enabled: z.boolean(),
   builtin: z.boolean(),
   main: z.string(),
