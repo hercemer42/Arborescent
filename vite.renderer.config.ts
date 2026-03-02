23c73e1cc6fd05b40110ce67254dd9b29168b66d
Brian <hercemer42@hotmail.com>
2026-03-02 13:30:29 +0100

    add linting workflow for github

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig(({ mode }) => ({
   define: {
     'process.env.NODE_ENV': JSON.stringify(mode),
   },
   plugins: [react()],
-  resolve: {},
   server: {
     watch: {
       ignored: ['**/coverage/**'],
